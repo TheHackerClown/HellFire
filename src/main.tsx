8ad410c3663e401ba9d4d0285b70fea05168cd54
TheHackerClown <dhruvprataps14@gmail.com>
2025-01-03 23:53:13 +0530

    Testing

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { MyProvider } from "./Hellfire.tsx";
+//import { MyProvider } from "./Hellfire.tsx";
 
+//Not Using Backend Engine until game is released
+//Which is <MyProvider></MyProvider>
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MyProvider>
       <App />
-    </MyProvider>
   </StrictMode>
 );
